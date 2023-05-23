33c699c090f9c9973b6f4ffefdf5c2f743f8d8a5
Joaovitor-RCB <jvrcb.dev@gmail.com>
2023-05-22 22:53:20 -0300

    Modal/js, buttons/css

--- index.js
+++ index.js
@@ -1,72 +1,53 @@
-//funçoes//
-    criaBtn = (Selector) => {
-    var produto = document.querySelector(Selector);
-    var buttonComprar = document.createElement("button");
-    const texto = document.createTextNode("COMPRAR");
-    buttonComprar.classList.add("btnC");
-    buttonComprar.appendChild(texto);
-    produto.appendChild(buttonComprar);
-};
+const corpo = document.querySelector("body");
 
-removeBtn = (Selector) => {
-    var produto = document.querySelector(Selector);
-    var btn = produto.querySelector(".btnC");
-    if (btn) {
-        btn.remove();
-    }};
-    
+criaModal = () => {
+  var modalTotal = document.createElement("div");
+  modalTotal.id = "modalTotal";
 
-    const corpo = document.querySelector("body");
+  var modalSobrenos = document.createElement("div");
+  modalSobrenos.id = "modalSobrenos";
+  modalTotal.appendChild(modalSobrenos);
 
-    criaModal = () => {
-    var modalTotal = document.createElement("div");
-    modalTotal.id = 'modalTotal';
+  var exit = document.createElement("div");
+  exit.id = "exit";
+  modalSobrenos.appendChild(exit);
 
-    
-    var modalSobrenos = document.createElement("div");
-    modalSobrenos.id = 'modalSobrenos';
-    modalTotal.appendChild(modalSobrenos);
-    
-    var exit = document.createElement("div");
-    exit.id = 'exit';
-    modalSobrenos.appendChild(exit);
+  var buttonExit = document.createElement("button");
+  var textoButton = document.createTextNode("X");
+  buttonExit.appendChild(textoButton);
+  exit.appendChild(buttonExit);
 
-    var buttonExit = document.createElement("button");
-    var textoButton = document.createTextNode("X");
-    buttonExit.appendChild( textoButton);
-    exit.appendChild(buttonExit);
+  var info = document.createElement("div");
+  info.id = "info";
+  modalSobrenos.appendChild(info);
 
-    var info = document.createElement("div");
-    info.id = ('info');
-    modalSobrenos.appendChild(info);
+  var h1 = document.createElement("h1");
+  var textoH1 = document.createTextNode("Atendimento");
+  h1.appendChild(textoH1);
+  info.appendChild(h1);
 
-    var h1 = document.createElement('h1');
-    var textoH1 = document.createTextNode("Atendimento");
-    h1.appendChild(textoH1);
-    info.appendChild(h1);
+  var paragrafo = document.createElement("p");
+  info.appendChild(paragrafo);
 
-    var paragrafo = document.createElement("p");
-    info.appendChild(paragrafo);
+  var textoP = document.createTextNode("Rua José de Alencar, 111, Centro ");
+  paragrafo.appendChild(textoP);
 
-    var textoP = document.createTextNode("Rua José de Alencar, 111, Centro ");
-    paragrafo.appendChild(textoP);
+  var quebra = document.createElement("br");
+  paragrafo.appendChild(quebra);
 
-    var quebra = document.createElement("br");
-    paragrafo.appendChild(quebra);
+  var textoP2 = document.createTextNode("CEP: 12209-000 | +55 (11) 3333-8000");
+  paragrafo.appendChild(textoP2);
 
-    var textoP2 = document.createTextNode("CEP: 12209-000 | +55 (11) 3333-8000");
-    paragrafo.appendChild(textoP2);
+  var buttonContato = document.createElement("button");
+  var textoButtonContato = document.createTextNode("ENTRAR EM CONTATO");
+  buttonContato.appendChild(textoButtonContato);
+  info.appendChild(buttonContato);
 
-    var buttonContato = document.createElement("button");
-    var textoButtonContato = document.createTextNode("ENTRAR EM CONTATO");
-    buttonContato.appendChild(textoButtonContato);
-    info.appendChild(buttonContato); 
+  corpo.appendChild(modalTotal);
 
-    corpo.appendChild(modalTotal);
-
-    fecharModal = () => {
-        var modalTotal = document.getElementById("modalTotal");
-        modalTotal.remove();
-    };
-    buttonExit.addEventListener("click", fecharModal);
+  fecharModal = () => {
+    var modalTotal = document.getElementById("modalTotal");
+    modalTotal.remove();
+  };
+  buttonExit.addEventListener("click", fecharModal);
 };
